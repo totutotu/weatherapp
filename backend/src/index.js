06e1f59606ff11a725d6f5d6881b68e8609e5ae2
totutotu <tuomo.torppa@gmail.com>
2018-06-03 20:36:14 +0300

    Started using OWM API, created initial coordinate requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
-const debug = require('debug')('weathermap');
+
+// const debug = require('debug')('weathermap');
+require('dotenv').config();
 
 const Koa = require('koa');
 const router = require('koa-router')();
@@ -6,8 +8,8 @@ const fetch = require('node-fetch');
 const cors = require('kcors');
 
 const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -19,14 +21,48 @@ const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
-router.get('/api/weather', async ctx => {
+const fetchWeatherByCoordinates = async (lon, lat) => {
+  const endpoint = `${mapURI}/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
+};
+
+const fetchForecast = async () => {
+  const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&cnt=2`;
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
+};
+
+router.get('/api/weatherinhelsinki', async ctx => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+router.get('/api/weatherbycoordinates', async ctx => {
+  if (ctx.request.query.lon && ctx.request.query.lat) {
+    const { lon, lat, } = ctx.request.query;
+    const weatherData = await fetchWeatherByCoordinates(lon, lat);
+    ctx.type = 'application/json; charset=utf-8';
+    ctx.body = weatherData.weather ? weatherData : {};
+  }
+});
+
+router.get('/api/forecast', async ctx => {
+  const weatherData = await fetchForecast();
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData.list
+    ? {
+      weather: weatherData.list[1].weather[0],
+      time: weatherData.list[1].dt_txt,
+    }
+    : {};
 });
 
 app.use(router.routes());
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,7 @@ const baseURL = process.env.ENDPOINT;
 
 const getWeatherFromApi = async () => {
   try {
-    const response = await fetch(`${baseURL}/weather`);
+    const response = await fetch(`${baseURL}/weatherinhelsinki`);
     return response.json();
   } catch (error) {
     console.error(error);
@@ -14,26 +14,83 @@ const getWeatherFromApi = async () => {
   return {};
 };
 
+const getForecastFromApi = async () => {
+  try {
+    const response = await fetch(`${baseURL}/forecast`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {};
+};
+
+const getCoordinateWeatherFromApi = async (lon, lat) => {
+  try {
+    const response = await fetch(`${baseURL}/weatherbycoordinates?lon=${lon}&lat=${lat}`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+  return {};
+};
+
 class Weather extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      icon: "",
+      iconNow: '',
+      iconSoon: '',
+      timeStamp: '',
+      lon: '',
+      lat: '',
+      location: 'Helsinki',
     };
   }
 
   async componentWillMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({icon: weather.icon.slice(0, -1)});
+    let weather;
+    let forecast;
+    if ('geolocation' in navigator) {
+      this.getPosition();
+    } else {
+      weather = await getWeatherFromApi();
+      forecast = await getForecastFromApi();
+    }
+    if (weather) this.setState({ icon: weather.icon.slice(0, -1) });
+    if (forecast) {
+      this.setState(
+        {
+          iconSoon: forecast.weather.icon.slice(0, -1),
+          timeStamp: forecast.time.slice(11, 16),
+        });
+    }
+  }
+
+  getPosition() {
+    window.navigator.geolocation.getCurrentPosition(async (pos) => {
+      if (pos.coords) {
+        this.setState({ lon: pos.coords.longitude, lat: pos.coords.latitude, location: pos.name });
+        const res = await getCoordinateWeatherFromApi(this.state.lon, this.state.lat);
+        this.setState({ location: res.name });
+      }
+    });
   }
 
   render() {
-    const { icon } = this.state;
+    const { icon, iconSoon, timeStamp, location } = this.state;
 
     return (
-      <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} /> }
+      <div>
+        <h2>Weather now in {location}</h2>
+        <div className="icon">
+          {icon && <img alt="weather_icon" src={`/img/${icon}.svg`} />}
+        </div>
+        <h2>Weather in {location} at {timeStamp}</h2>
+        <div className="icon">
+          {icon && <img alt="weather_icon" src={`/img/${iconSoon}.svg`} />}
+        </div>
       </div>
     );
   }
